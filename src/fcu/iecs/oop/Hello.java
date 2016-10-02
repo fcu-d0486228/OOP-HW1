f2bfac0d0a6ad20725915633a4b8dec41925ad17
d0486228 <d0486228@fcu.edu.tw>
2016-10-02 16:43:11 +0800

    String Replace

--- src/fcu/iecs/oop/Hello.java
+++ src/fcu/iecs/oop/Hello.java
@@ -3,8 +3,10 @@ package fcu.iecs.oop;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello OOP");
-
+		String lyrics = "Let it go! Let it go! Cannot hold it back anymore";
+		String lyrics2 = lyrics.replace("it", "\"her\"");
+		String lyrics3 = lyrics2.replace("Cannot", "Can\'t");
+		System.out.println(lyrics3);
 	}
 
 }
